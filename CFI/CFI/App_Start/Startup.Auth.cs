1ded5b49719cc53cc3661cdb501e0accfd4a6147
Maximiliano Herrera <herreramaxi@gmail.com>
2016-10-02 00:04:05 +0100

    Logging with third party login providers: Google, LinkedIn

--- CFI/CFI/App_Start/Startup.Auth.cs
+++ CFI/CFI/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using CFI.Models;
+using Owin.Security.Providers.LinkedIn;
+using System.Configuration;
 
 namespace CFI
 {
@@ -34,7 +36,7 @@ namespace CFI
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
@@ -45,24 +47,15 @@ namespace CFI
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            app.UseGoogleAuthentication(
+                clientId: ConfigurationManager.AppSettings["GoogleClientId"],
+                clientSecret: ConfigurationManager.AppSettings["GoogleClientSecret"]);
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
-
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseLinkedInAuthentication(new LinkedInAuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["LinkedInClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["LinkedInClientSecret"],
+            });
         }
     }
 }
