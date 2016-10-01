3356db08410156e5a53164899fc4f81db7277d58
Maximiliano Herrera <herreramaxi@gmail.com>
2016-10-01 22:18:48 +0100

    CFI project is uploaded :)

---
+++ CFI/CFI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CFI.Startup))]
+namespace CFI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
